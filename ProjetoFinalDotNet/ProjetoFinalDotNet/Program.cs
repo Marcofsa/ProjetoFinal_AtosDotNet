e3b4467c20a448f2f07ce485faf515067d938381
Marcofsa <87910108+Marcofsa@users.noreply.github.com>
2022-12-07 10:34:03 -0300

    In progress

--- ProjetoFinalDotNet/ProjetoFinalDotNet/Program.cs
+++ ProjetoFinalDotNet/ProjetoFinalDotNet/Program.cs
@@ -22,18 +22,13 @@ namespace ProjetoFinalDotNet
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-
             app.Run();
         }
     }
